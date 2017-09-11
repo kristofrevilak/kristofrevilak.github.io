f98f2b9e821607dd4901b3dda55210dbffbc85d4
Kiko <kristofrevilak@gmail.com>
2017-09-11 18:58:07 +0200

    11.9.2017 edited bastler for better mobile readability

--- bastler/main.js
+++ bastler/main.js
@@ -6,9 +6,14 @@ $(function(){
     $(".nav-menu-mobile").fadeOut('slow', function() {});
   });
   $(".exit").hover(function() {
-    console.log("kiko");
     $(".exit").animate({borderColor: 'white',color: 'white'},200,function(){});
   }, function() {
     $(".exit").animate({borderColor: 'black',color: 'black'},200,function(){});
+  });
+  $(".main-section-see-more").click(function() {
+    console.log("kiko");
+    $('html, body').animate({
+      scrollTop: $("#see-more").offset().top
+    }, 800);
   });
 })
