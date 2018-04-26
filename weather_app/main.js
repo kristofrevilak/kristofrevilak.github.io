36aa337347365285699a65183c93d28253eec688
Kiko <kristofrevilak@gmail.com>
2018-04-26 21:16:38 +0200

    weather_app apis added

--- weather_app/main.js
+++ weather_app/main.js
@@ -1,4 +1,8 @@
-var url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=fc9497a01b3d4429206db1740442ef04&";
+let weather = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=Bo2eoe3z7pXFlTeZLRFGkCJXxITS9tSq&q="
+let weather_end = "&details=true"
+
+let img = "https://pixabay.com/api/?key=8814402-dfba581d7d4bbd350ea0d6f8b&q=";
+let img_end = "&image_type=photo&pretty=true";
 
 $(document).ready(function() {
   getLocation();
@@ -11,9 +15,9 @@ function getLocation() {
 }
 
 function getWeather(position){
-  url += "lon=" + position.coords.longitude + "&lat=" + position.coords.latitude;
+  weather += position.coords.latitude + "%2C" + position.coords.longitude + weather_end;
   $.ajax({
-    url: url,
+    url: weather,
 
     type: "GET",
 
@@ -21,23 +25,41 @@ function getWeather(position){
 
     cache: true,
 
-    data: {
-      format: "jsonp"
-    },
     success: showWeather
   });
 
 }
 
+function getImage(city){
+  img += city + img_end;
+  console.log(img);
+  $.ajax({
+    url: img,
+
+    type: "GET",
+
+    dataType: 'jsonp',
+
+    cache: true,
+
+    success: showImage
+  });
+}
+
 function showWeather(result){
   console.log(result);
-  var city = result.city.name;
-  var country = result.city.country;
-  var weather = result.list[0].weather[0].description;
-  var wind = parseInt(result.list[0].wind.speed) * 1.609344;
-  var temp = Math.ceil(parseInt(result.list[0].main.temp) - 273.15);
+  var city = result.AdministrativeArea.EnglishName;
+  // var country = result.city.country;
+  // var weather = result.list[0].weather[0].description;
+  // var wind = parseInt(result.list[0].wind.speed) * 1.609344;
+  // var temp = Math.ceil(parseInt(result.list[0].main.temp) - 273.15);
 
 
   
-  $("#basic").html("It's " + temp + "°C in " + city + ", " + country);
+  // $("#basic").html("It's " + temp + "°C in " + city + ", " + country);
+  getImage(city);
+}
+
+function showImage(images){
+  console.log(images.hits[0]);
 }
