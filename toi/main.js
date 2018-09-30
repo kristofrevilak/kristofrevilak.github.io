7e691a5f35d62144be1623d73e3c60dac6db608e
Kiko <kristofrevilak@gmail.com>
2018-09-30 20:24:19 +0200

    another day at the office

--- toi/main.js
+++ toi/main.js
@@ -17,6 +17,11 @@ let $expand_div = $('.expand-div');
 let $expand_arr = $('.member-arrow img');
 let $expand_content = $('.member-expand-content');
 
+let $single_fact = $(".single-fact");
+let $single_fact_arr = $(".academy-arrow")[0];
+
+let $expand_btn = $(".expand");
+
 
 $exploring_btn.click((e)=>{
 	e.preventDefault();
@@ -46,17 +51,50 @@ $expand_div.click((e)=>{
 	if(imgg.attributes.src.nodeValue == "arrow_down.png"){
 		let change_img_tween = TweenMax.set(imgg,{attr:{src:"arrow_up.png"}});
 		let display_text_tween = TweenMax.set($("#" + e.currentTarget.id).next().next(),{css:{display:"block"}});
-		let display_box_tween = TweenMax.set(blue_box,{css:{display:"block"}});
+		if( screen.width >= 750 ){
+			let display_box_tween = TweenMax.set(blue_box,{css:{display:"block"}});
+		}
+			
 	} else{
 		let change_img_tween = TweenMax.set(imgg,{attr:{src:"arrow_down.png"}});
 		let hide_text_tween = TweenMax.set($("#" + e.currentTarget.id).next().next(),{css:{display:"none"}});
-		let display_box_tween = TweenMax.set(blue_box,{css:{display:"none"}});
+		if( screen.width >= 750 ){
+			let display_box_tween = TweenMax.set(blue_box,{css:{display:"none"}});
+		}
+			
 	}
 
 	// let start_h_tween = TweenMax.to($start_h,1,{
 	// 	ease: Power1.easeInOut,
 	// 	css: {opacity: 1}
 	// });
+});
+
+$single_fact.click((e)=>{
+	e.preventDefault();
+	let data_id = $(e.currentTarget).attr("data-id");
+	let ele = $('*[data-id=' + data_id + ']');
+	if(!ele.hasClass("active")){
+		$(".active").removeClass("active");
+		ele.addClass("active");
+		let nheight = $('*[data-id="af0"]').offset().top;
+		let heightt = Math.ceil($('*[data-id=' + data_id + ']').offset().top - nheight) + 20;
+		$(".bullets-change ul").hide();
+		$("#" + data_id).css("display","block");
+	}
+});
+
+$expand_btn.click((e)=>{
+	e.preventDefault();
+	let data_id = $(e.currentTarget).attr("data-id");
+	let ele = $('*[data-id=' + data_id + ']');
+	if(!ele.hasClass("activated")){
+		ele.addClass("activated");
+		$("#" + data_id).css("display","block");
+	}else{
+		ele.removeClass("activated");
+		$("#" + data_id).css("display","none");
+	}
 });
 
 $explore_btn.mouseenter((e)=>{
@@ -72,6 +110,7 @@ $explore_btn.mouseenter((e)=>{
 		css: {marginTop: 10}
 	});
 });
+
 
 $( document ).ready(() => {
 	let start_h_tween = TweenMax.to($start_h,1,{
